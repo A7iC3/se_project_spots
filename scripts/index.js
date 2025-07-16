271b11b69ebf31e3f0e41c683187b6b0b4424478
Sarah Alice <sagostinacchio@hotmail.com>
2025-07-16 11:51:36 -0400

    Spots Stage 6

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,32 @@ const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
 const newPostName = newPostModal.querySelector("#caption");
 const newPostLink = newPostModal.querySelector("#imageLink");
 const newPostForm = newPostModal.querySelector(".modal__form");
+let initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
 function editProfileSetDefault() {
   editProfileName.value = currentProfileName.textContent;
@@ -54,3 +80,7 @@ newPostCloseBtn.addEventListener("click", () => {
 });
 
 newPostForm.addEventListener("submit", newPostSave);
+
+initialCards.forEach((item) => {
+  console.log(item.name);
+});
